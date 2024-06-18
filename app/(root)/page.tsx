da5ff26809cada0d30df89c7c2daa9b49619efe1
Charon <omaralmoctar.coulibaly@edu.ece.fr>
2024-06-18 15:47:39 +0200

    small changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -34,7 +34,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           </div>
 
           <Image
-            src="/assets/images/hero.png"
+            src="/assets/images/10821625.jpg"
             alt="hero"
             width={1000}
             height={1000}
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -13,7 +13,11 @@ export const Header = () => {
         <header className='w-full border-b'>
             <div className='wrapper flex items-center justify-between'>
                 <Link href="/" className='w-36'>
-                    <Image src="/assets/images/logo.svg" width={128} height={38} alt='Event logo' />
+                    <Image
+                        src="/assets/images/diamond.png"
+                        width={130}
+                        height={50}
+                        alt='Event logo' />
                 </Link>
                 <SignedIn>
                     <nav className='md:flex-between hidden w-full max-w-xs'>
