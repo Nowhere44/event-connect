d1d19cb8e36c14d5deede8f5fd3140cb020788e8
Charon <omaralmoctar.coulibaly@edu.ece.fr>
2024-06-18 15:08:40 +0200

    almost all the folder remain only stripe

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -15,14 +15,26 @@ const MobileNav = () => {
     return (
         <nav className="md:hidden">
             <Sheet>
-                <SheetTrigger className="align-middle">Open</SheetTrigger>
-                <Image src="/assets/icons/menu.svg" width={24} height={24} alt="menu" /> <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
-                    <Image src="/assets/images/logo.svg" width={128} height={38} alt='logo' />
+                <SheetTrigger className="align-middle">
+                    <Image
+                        src="/assets/icons/menu.svg"
+                        alt="menu"
+                        width={24}
+                        height={24}
+                        className="cursor-pointer"
+                    />
+                </SheetTrigger>
+                <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+                    <Image
+                        src="/assets/images/logo.svg"
+                        alt="logo"
+                        width={128}
+                        height={38}
+                    />
                     <Separator className="border border-gray-50" />
                     <NavItems />
                 </SheetContent>
             </Sheet>
-
         </nav>
     )
 }
